9cbabcdbe484b1dc796f49e7713df3639ea615f5
Vladimir Bushev <voldemarbush@gmail.com>
2024-01-22 21:40:09 +0300

    feat: add 04-copy-directory task

---
+++ 04-copy-directory/index.js
@@ -0,0 +1,38 @@
+const { exit } = process;
+const fs = require('fs');
+const path = require('path');
+const srcDir = path.join(__dirname, 'files');
+const destDir = path.join(__dirname, 'files-copy');
+
+fs.mkdir(destDir, (error) => {
+  if (error) {
+    console.error(`Error: ${error.message}`);
+    exit(1);
+  }
+
+  fs.readdir(srcDir, (error, files) => {
+    if (error) {
+      console.error(`Error: ${error.message}`);
+      exit(1);
+    }
+
+    files.forEach((file) => {
+      const srcPath = path.join(srcDir, file);
+      const destPath = path.join(destDir, file);
+
+      fs.readFile(srcPath, (error, data) => {
+        if (error) {
+          console.error(`Error: ${error.message}`);
+          exit(1);
+        }
+
+        fs.writeFile(destPath, data, (error) => {
+          if (error) {
+            console.error(`Error: ${error.message}`);
+            exit(1);
+          }
+        });
+      });
+    });
+  });
+});
