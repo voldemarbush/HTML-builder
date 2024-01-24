370bba0b5c9d2ed31c40c7ef203aee4f9dad1185
Vladimir Bushev <voldemarbush@gmail.com>
2024-01-24 15:16:59 +0300

    fix: mkdir error in 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -4,33 +4,39 @@ const path = require('path');
 const srcDir = path.join(__dirname, 'files');
 const destDir = path.join(__dirname, 'files-copy');
 
-fs.mkdir(destDir, (error) => {
+fs.rm(destDir, { recursive: true, force: true }, (error) => {
   if (error) {
-    console.error(`Error: ${error.message}`);
+    console.error('Error deleting the folder:', error);
     exit(1);
   }
-
-  fs.readdir(srcDir, (error, files) => {
+  fs.mkdir(destDir, (error) => {
     if (error) {
       console.error(`Error: ${error.message}`);
       exit(1);
     }
 
-    files.forEach((file) => {
-      const srcPath = path.join(srcDir, file);
-      const destPath = path.join(destDir, file);
+    fs.readdir(srcDir, (error, files) => {
+      if (error) {
+        console.error(`Error: ${error.message}`);
+        exit(1);
+      }
 
-      fs.readFile(srcPath, (error, data) => {
-        if (error) {
-          console.error(`Error: ${error.message}`);
-          exit(1);
-        }
+      files.forEach((file) => {
+        const srcPath = path.join(srcDir, file);
+        const destPath = path.join(destDir, file);
 
-        fs.writeFile(destPath, data, (error) => {
+        fs.readFile(srcPath, (error, data) => {
           if (error) {
             console.error(`Error: ${error.message}`);
             exit(1);
           }
+
+          fs.writeFile(destPath, data, (error) => {
+            if (error) {
+              console.error(`Error: ${error.message}`);
+              exit(1);
+            }
+          });
         });
       });
     });
